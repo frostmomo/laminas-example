f4e42786a56f6db7b2bb63aacb67b8a373f5cc46
frostmomo <frostmomo4@gmail.com>
2023-09-12 07:25:46 +0700

    added to gh

---
+++ config/autoload/global.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'db' => [
+        'driver' => 'Pdo',
+        'dsn' => 'mysql:dbname=laminasdb;host=localhost',
+        'username' => 'root',
+        'password' => '',
+    ],
+];
---
+++ config/modules.config.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * List of enabled modules for this application.
+ *
+ * This should be an array of module namespaces used in the application.
+ */
+return [
+    'Laminas\Mvc\I18n',
+    'Laminas\Form',
+    'Laminas\I18n',
+    'Laminas\Log',
+    'Laminas\InputFilter',
+    'Laminas\Filter',
+    'Laminas\Hydrator',
+    'Laminas\Db',
+    'Laminas\Router',
+    'Laminas\Validator',
+    'Laminas\DeveloperTools',
+    'Application',
+    'Album',
+];
